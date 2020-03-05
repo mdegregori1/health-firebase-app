7302b00f5b2e0af952375f01e20ffbefc9d903e6
Mauricio Degregori <degregori2@gmail.com>
2020-03-04 19:33:56 -0600

    added materialize css

---
+++ maris-health/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <nav className="nav-wrapper blue darken-3">
+            <div className="container">
+                <Link to ='/' className="brand-logo">MarisHealth</Link>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar;
