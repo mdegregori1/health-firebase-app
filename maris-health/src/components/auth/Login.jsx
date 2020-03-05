aad5b4c565209b614b77892a7c54f1650f735034
Mauricio Degregori <degregori2@gmail.com>
2020-03-05 00:18:26 -0600

    login component done minus backend connection

--- maris-health/src/components/auth/Login.jsx
+++ maris-health/src/components/auth/Login.jsx
@@ -1,22 +1,42 @@
 import React, { useState } from "react";
 
 const Login = () => {
+    const [userlogin, setUserlogin] = useState({
+        email: '', 
+        password: ''
+    })
+
+
+    const handleChange = e => {
+        setUserlogin({ ...userlogin, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log(userlogin)
+    }
+
     return (
         <div className="container">
-            <form className="white">
+            <form onSubmit={handleSubmit} className="white">
                 <h5 className="grey-text text-darken-3">
                     Login
                 </h5>
                 <div className="input-field">
                     <label htmlFor = "email">Email</label>
-                    <input type="email" id="email"/>
+                    <input type="email" name="email"
+                    onChange={handleChange}
+                    />
+                    
                 </div>
                 <div className="input-field">
                     <label htmlFor = "password">Password</label>
-                    <input type="password" id="password"/>
+                    <input type="password" name="password"
+                     onChange={handleChange}
+                    />
                 </div>
                 <div className="input-field">
-                    <button className="btn blue darken-2 text black blue-text text-lighten-4">Login</button>
+                    <button onClick={handleSubmit}className="btn blue darken-2 text black blue-text text-lighten-4">Login</button>
                 </div>
             </form>
         </div>
